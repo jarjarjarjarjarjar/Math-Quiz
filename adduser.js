c168e852a06a742205e9897ec536f06092bd4374
jarjarjarjarjarjar <75090491+jarjarjarjarjarjar@users.noreply.github.com>
2021-07-31 12:20:41 -0500

    Delete adduser.js

--- adduser.js
+++
@@ -1,8 +0,0 @@
-function adduser()
-{
-        player1name = document.getElementById("1nameinput").value
-        player2name = document.getElementById("2nameinput").value
-        localStorage.setItem("player1name",player1name)
-        localStorage.setItem("player2name",player2name)
-        window.location="Quiz.html"    
-}
